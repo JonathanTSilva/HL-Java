23abf041321796073f8789b71171d76a9d925045
Jonathan Silva <jonathan.silva@smar.com.br>
2022-09-08 07:22:47 -0300

    feat: implement update department method
    Checklist:
    - In DepartmentListController
        - Create new attribute: TableColumn<Department, Department> tableColumnEDIT;
        - Create initEditButtons method
        - In updateTableViewData, call initEditButtons
    - In DepartmentList.fxml
        - Include new table column
        - Associate id

--- Build/Course-JCPOO/Project05-CRUD/src/gui/DepartmentFormController.java
+++ Build/Course-JCPOO/Project05-CRUD/src/gui/DepartmentFormController.java
@@ -28,25 +28,30 @@ public class DepartmentFormController implements Initializable {
     private Department entity;
     private DepartmentService service;
     private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
-    
-    @FXML private TextField txtId;
-    @FXML private TextField txtName;
-    @FXML private Label labelErrorName;
-    @FXML private Button btSave;
-    @FXML private Button btCancel;
-    
+
+    @FXML
+    private TextField txtId;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private Label labelErrorName;
+    @FXML
+    private Button btSave;
+    @FXML
+    private Button btCancel;
+
     public void setDepartment(Department entity) {
         this.entity = entity;
     }
-    
+
     public void setDepartmentService(DepartmentService service) {
         this.service = service;
     }
-    
+
     public void subscribeDataChangeListener(DataChangeListener listener) {
         dataChangeListeners.add(listener);
     }
-    
+
     @FXML
     public void onBtSaveAction(ActionEvent event) {
         if (entity == null) {
@@ -64,9 +69,9 @@ public class DepartmentFormController implements Initializable {
             setErrorMessages(e.getErrors());
         } catch (DbException e) {
             Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
-        } 
+        }
     }
-    
+
     private void notifyDataChangeListeners() {
         for (DataChangeListener listener : dataChangeListeners) {
             listener.onDataChanged();
@@ -91,17 +96,17 @@ public class DepartmentFormController implements Initializable {
     public void onBtCancelAction(ActionEvent event) {
         Utils.currentStage(event).close();
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initializeNodes();
     }
-    
+
     private void initializeNodes() {
         Constraints.setTextFieldInteger(txtId);
         Constraints.setTextFieldMaxLength(txtName, 30);
     }
-    
+
     public void updateFormData() {
         if (entity == null) {
             throw new IllegalStateException("Entity was null");
@@ -109,7 +114,7 @@ public class DepartmentFormController implements Initializable {
         txtId.setText(String.valueOf(entity.getId()));
         txtName.setText(entity.getName());
     }
-    
+
     private void setErrorMessages(Map<String, String> errors) {
         Set<String> fields = errors.keySet();
         if (fields.contains("name")) {
