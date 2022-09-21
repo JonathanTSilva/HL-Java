8b027a377dda0fd2ef918eea871ec07ac683febb
Jonathan Silva <jonathan.silva@smar.com.br>
2022-09-21 07:29:10 -0300

    build(project06): init repository

---
+++ Build/Course-JCPOO/Project06-RESTFUL/src/main/java/com/smar/stsmongo/StsmongoApplication.java
@@ -0,0 +1,13 @@
+package com.smar.stsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsmongoApplication.class, args);
+	}
+
+}
---
+++ Build/Course-JCPOO/Project06-RESTFUL/src/test/java/com/smar/stsmongo/StsmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smar.stsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
