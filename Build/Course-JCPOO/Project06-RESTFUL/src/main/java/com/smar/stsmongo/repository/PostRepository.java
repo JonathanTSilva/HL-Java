0a994d5441e3cc7344d7517d4c8413cf6cddfb4d
Jonathan Silva <jonathan.silva@smar.com.br>
2022-09-22 08:44:17 -0300

    feat: implement multicriteria query
    Query:
    "Fetch posts containing a given string anywhere (in the title, body or comments) and in a given date range"
    
    Checklist:
    - In PostRepository, create the query method
    - In PostService, create the query method
    - In the URL utility class, create methods to handle received dates
    - In PostResource, implement the endpoint

--- Build/Course-JCPOO/Project06-RESTFUL/src/main/java/com/smar/stsmongo/repository/PostRepository.java
+++ Build/Course-JCPOO/Project06-RESTFUL/src/main/java/com/smar/stsmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.smar.stsmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -18,4 +19,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
     // Consulta simples com query methods
     List<Post> findByTitleContainingIgnoreCase(String text);
     
+    // Consulta com vários critérios
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
