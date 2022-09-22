69cc19a8d18babb63574b49ea81ba50c38b96aed
Jonathan Silva <jonathan.silva@smar.com.br>
2022-09-22 08:30:07 -0300

    feat: implement simple query with @Query
    References:
    https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/
    https://docs.spring.io/spring-data/data-document/docs/current/reference/html/
    https://docs.mongodb.com/manual/reference/operator/query/regex/
    
    Query:
    "Fetch posts containing a given string in the title"
    
    Checklist:
    - In PostRepository, make the alternative implementation of the query
    - In PostService, update the query call

--- Build/Course-JCPOO/Project06-RESTFUL/src/main/java/com/smar/stsmongo/repository/PostRepository.java
+++ Build/Course-JCPOO/Project06-RESTFUL/src/main/java/com/smar/stsmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.smar.stsmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.smar.stsmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.smar.stsmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    // Consulta simples com @Query
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+    
+    // Consulta simples com query methods
     List<Post> findByTitleContainingIgnoreCase(String text);
     
 }
